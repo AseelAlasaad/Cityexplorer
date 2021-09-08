03765020cc7ea397a03bc8078e90bdadf1a5f7e6
Aseel Alasaad <aseelalasaad@gmail.com>
2021-09-08 16:39:12 +0300

    fixed

--- src/Component/Movie.js
+++ src/Component/Movie.js
@@ -1,41 +1,52 @@
-import React, { Component } from 'react';
-
-class Movie extends Component {
+import React from 'react';
+import { Card, Col,Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class Movie extends React.Component {
     render() {
-        return (
-            
-          <>
-          {this.props.movieFlage&&this.props.data.map(item=>{
-             <>
-             <p> {item.title} </p>
-              <p>{item.overview}</p> 
-              <p>
-                  {  item.vote_average}
-              </p>
-            
-              <p>
-                  {item.vote_count}
-              </p>
-            
-            <p>
-                  { item.popularity}
-              </p>
-          
-           <p>
-                  {item.release  }
-              </p>
-              
-    
-            
-             </>
 
-          })}
-        
-          </>
-       
-        )
-         }        
-   
+        return (
+
+            <> <Row xs={1} md={3} className="g-4">
+                {
+                    this.props.dataMovie.map((element, key) => (
+                        <>
+
+                           <Col>
+                            <Card style={{ width: '18rem' }}>
+
+                                <Card.Body>
+                                    <Card.Title>Movies</Card.Title>
+                                    <Card.Text>
+                                        <p>Title    : {element.title}</p>
+                                        <p>overview : {element.overview}</p>
+                                        <p>
+                                        vote_average:  {element.vote_average}
+                                        </p>
+
+                                        <p>
+                                        vote_count  :  {element.vote_count}
+                                        </p>
+
+                                        <p>
+                                        popularity :  {element.popularity}
+                                        </p>
+
+                                    </Card.Text>
+
+                                </Card.Body>
+                            </Card>
+                            </Col>
+                        
+                        </>
+
+                    ))}</Row>
+
+
+            </>
+
+        );
+    }
+
 }
 
 export default Movie;
